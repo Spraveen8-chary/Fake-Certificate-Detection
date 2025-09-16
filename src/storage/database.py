498efd7d9f364f99645066c2c635af5db499fc05
S Praveen <108536707+Spraveen8-chary@users.noreply.github.com>
2025-09-16 18:46:40 +0530

    Update database.py

--- src/storage/database.py
+++ src/storage/database.py
@@ -45,6 +45,7 @@ class SupabaseDB:
     | logo_embeddings      | bytea                       |
     | qr_code_cipher       | text                        |
     | created_at           | timestamp without time zone |
+    | clg_code             | text                        |
 
 
 
@@ -65,7 +66,7 @@ class SupabaseDB:
 
 
     📌 `universities`
-
+    ______________________________________________________
     | Column                | Data Type                   |
     | --------------------- | --------------------------- |
     | univ_id               | uuid                        |
@@ -77,7 +78,7 @@ class SupabaseDB:
     | logo_embeddings       | bytea                       |
     | stamp_embeddings      | bytea                       |
     | created_at            | timestamp without time zone |
-
+    | university_website    | text                        |
 
     📌 `verification_logs`
 
@@ -90,7 +91,16 @@ class SupabaseDB:
     | reason       | text                        |
     | verified_at  | timestamp without time zone |
 
-
+    
+    📌 `affiliate_colleges`
+    | Column        | Data Type                   |
+    | ------------- | --------------------------- |
+    | clg_code      | text                        |
+    | clg_name      | text                        |
+    | clg_address   | text                        |
+    | univ_id       | uuid                        |
+    | created_at    | timestamp without time zone |
+    | clg_web       | text                        |
     """
 
     def __init__(self):
@@ -248,7 +258,9 @@ class SupabaseDB:
             logger.error(f"❌ Failed to validate admin login: {e}")
             return False
 
-    # ================================== Admin Management ===================================
+    # ================================== End of Admin Management ===================================
+
+    # ================================== Student Management ===================================
 
     def insert_student(self, name:str, email:str, password:str, roll_no:str, dob:str, univ_id:str, passed_out_year:int):
         """Insert a new student into the students table"""
@@ -277,6 +289,298 @@ class SupabaseDB:
             logger.error(f"❌ Failed to insert student: {e}")
 
 
+    def get_student(self, student_id:int):
+        """Fetch a student by ID"""
+        try:
+            query = f"SELECT student_id, name, email, roll_no, dob, univ_id, passed_out_year FROM students WHERE student_id = {student_id};"
+            result = self.run_query(query, fetch_one=True)
+            return result
+        except Exception as e:
+            logger.error(f"❌ Failed to fetch student: {e}")
+            return None
+
+    def update_student(self, student_id:int, name:str, email:str, roll_no:str, dob:str, univ_id:str, passed_out_year:int):
+        """Update a student's details"""
+        try:
+            query = f"""
+            UPDATE students
+            SET name = '{name}', email = '{email}', roll_no = '{roll_no}', dob = '{dob}', univ_id = '{univ_id}', passed_out_year = {passed_out_year}
+            WHERE student_id = {student_id};
+            """
+            self.run_query(query)
+            logger.info("✅ Student updated successfully!")
+        except Exception as e:
+            logger.error(f"❌ Failed to update student: {e}")
+
+
+    def delete_student_by_id(self, student_id:int):
+        """Delete a student by ID"""
+        try:
+            query = f"DELETE FROM students WHERE student_id = {student_id};"
+            self.run_query(query)
+            logger.info("✅ Student deleted successfully!")
+        except Exception as e:
+            logger.error(f"❌ Failed to delete student: {e}")
+
+
+    def delete_student_by_mail(self, email:str):
+        """Delete a student by email"""
+        try:
+            query = f"DELETE FROM students WHERE email = '{email}';"
+            self.run_query(query)
+            logger.info("✅ Student deleted successfully!")
+        except Exception as e:
+            logger.error(f"❌ Failed to delete student: {e}")
+
+    
+    def display_all_students_certificates_by_id(self, student_id:int):
+        """Display all certificates of a student by student ID"""
+        try:
+            query = f"""
+            SELECT c.cert_id, c.roll_no, c.student_name_hash, c.dob_hash, c.gpa_hash, c.batch_year, c.issued_date, c.file_url, c.qr_code_cipher, c.image_hash
+            FROM certificates c
+            WHERE c.student_id = {student_id};
+            """
+            result = self.run_query(query, fetch_all=True)
+            return result
+        except Exception as e:
+            logger.error(f"❌ Failed to fetch certificates: {e}")
+            return []
+
+    def display_all_students_certificates(self):
+        """Display all certificates of all students"""
+        try:
+            query = f"""
+            SELECT c.cert_id, c.roll_no, c.student_name_hash, c.dob_hash, c.gpa_hash, c.batch_year, c.issued_date, c.file_url, c.qr_code_cipher, c.image_hash
+            FROM certificates c JOIN students s ON c.student_id = s.student_id group by s.email,c.cert_id;
+            """
+            result = self.run_query(query, fetch_all=True)
+            return result
+        except Exception as e:
+            logger.error(f"❌ Failed to fetch certificates: {e}")
+            return []
+        
+    # ================================= End of Student Management ===================================
+
+    # ================================= University Management ===================================
+
+    def insert_university(self, name:str, address:str, private_key:str):
+        """Insert a new university into the universities table"""
+        try:
+            
+            hash_private_key = bcrypt.hashpw(private_key.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+            public_key = private_key[::-1] 
+            hash_public_key = bcrypt.hashpw(public_key.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+            query = f"""
+            INSERT INTO universities (name, address, private_key, public_key)
+            VALUES ('{name.lower().strip()}', '{address}', '{hash_private_key}', '{hash_public_key}');
+            """
+            self.run_query(query)
+            logger.info("✅ University inserted successfully!")
+        except Exception as e:
+            logger.error(f"❌ Failed to insert university: {e}")
+
+    def get_university(self, univ_id:int):
+        """Fetch a university by ID"""
+        try:
+            query = f"SELECT univ_id, name, address, created_at FROM universities WHERE univ_id = {univ_id};"
+            result = self.run_query(query, fetch_one=True)
+            return result
+        except Exception as e:
+            logger.error(f"❌ Failed to fetch university: {e}")
+            return None
+    
+    def get_university_by_private_key(self, private_key:str):
+        """Fetch a university by private key"""
+        try:
+            hash_private_key = bcrypt.hashpw(private_key.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+            query = f"SELECT univ_id, name, address, created_at FROM universities WHERE private_key = '{hash_private_key}';"
+            result = self.run_query(query, fetch_one=True)
+            return result
+        except Exception as e:
+            logger.error(f"❌ Failed to fetch university: {e}")
+            return None
+        
+    def update_university_by_univ_id(self, univ_id:int, name:str, address:str, private_key:str):
+        """Update a university's details"""
+        try:
+            hash_private_key = bcrypt.hashpw(private_key.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+            public_key = private_key[::-1] 
+            hash_public_key = bcrypt.hashpw(public_key.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+            query = f"""
+            UPDATE universities
+            SET name = '{name}', address = '{address}', private_key = '{hash_private_key}', public_key = '{hash_public_key}'
+            WHERE univ_id = {univ_id};
+            """
+            self.run_query(query)
+            logger.info("✅ University updated successfully!")
+        except Exception as e:
+            logger.error(f"❌ Failed to update university: {e}")
+
+    def update_university_by_private_key(self, private_key:str, name:str, address:str):
+        """Update a university's details by private key"""
+        try:
+            hash_private_key = bcrypt.hashpw(private_key.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+            public_key = private_key[::-1] 
+            hash_public_key = bcrypt.hashpw(public_key.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+            query = f"""
+            UPDATE universities
+            SET name = '{name}', address = '{address}', private_key = '{hash_private_key}', public_key = '{hash_public_key}'
+            WHERE private_key = '{hash_private_key}';
+            """
+            self.run_query(query)
+            logger.info("✅ University updated successfully!")
+        except Exception as e:
+            logger.error(f"❌ Failed to update university: {e}")
+
+    def delete_university_by_univ_id(self, univ_id:int):
+        """Delete a university by ID"""
+        try:
+            query = f"DELETE FROM universities WHERE univ_id = {univ_id};"
+            self.run_query(query)
+            logger.info("✅ University deleted successfully!")
+        except Exception as e:
+            logger.error(f"❌ Failed to delete university: {e}")
+
+    def delete_university_by_private_key(self, private_key:str):
+        """Delete a university by private key"""
+        try:
+            hash_private_key = bcrypt.hashpw(private_key.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+            query = f"DELETE FROM universities WHERE private_key = '{hash_private_key}';"
+            self.run_query(query)
+            logger.info("✅ University deleted successfully!")
+        except Exception as e:
+            logger.error(f"❌ Failed to delete university: {e}")
+
+
+    def get_university_private_key_by_univ_id(self, univ_id:int) -> str:
+        """Fetch a university's private key by ID"""
+        try:
+            query = f"SELECT private_key FROM universities WHERE univ_id = {univ_id};"
+            result = self.run_query(query, fetch_one=True)
+            return result[0] if result else None
+        except Exception as e:
+            logger.error(f"❌ Failed to fetch private key: {e}")
+            return None
+    
+    def get_university_private_key_by_name(self, name:str) -> str:
+        """Fetch a university's private key by name"""
+        try:
+            query = f"SELECT private_key FROM universities WHERE name = '{name.lower()}';"
+            result = self.run_query(query, fetch_one=True)
+            return result[0] if result else None
+        except Exception as e:
+            logger.error(f"❌ Failed to fetch private key: {e}")
+            return None
+        
+    def get_university_univ_id_by_name(self, name:str) -> int:
+        """Fetch a university's ID by name"""
+        try:
+            query = f"SELECT univ_id FROM universities WHERE name = '{name.lower()}';"
+            result = self.run_query(query, fetch_one=True)
+            return result[0] if result else None
+        except Exception as e:
+            logger.error(f"❌ Failed to fetch university ID: {e}")
+            return None
+    
+    def get_university_univ_id_by_private_key(self, private_key:str) -> int:
+        """Fetch a university's ID by private key"""
+        try:
+            hash_private_key = bcrypt.hashpw(private_key.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+            query = f"SELECT univ_id FROM universities WHERE private_key = '{hash_private_key}';"
+            result = self.run_query(query, fetch_one=True)
+            return result[0] if result else None
+        except Exception as e:
+            logger.error(f"❌ Failed to fetch university ID: {e}")
+            return None
+
+
+
+    def get_university_count(self) -> int:
+        """Get total number of universities"""
+        try:
+            query = "SELECT COUNT(*) FROM universities;"
+            result = self.run_query(query, fetch_one=True)
+            return result[0] if result else 0
+        except Exception as e:
+            logger.error(f"❌ Failed to count universities: {e}")
+            return 0
+    
+    def get_university_students_by_univ_id(self, univ_id:int):
+        """Fetch all students of a university by university ID"""
+        try:
+            query = f"""
+            SELECT student_id, name, email, roll_no, dob, passed_out_year, created_at
+            FROM students
+            WHERE univ_id = {univ_id};
+            """
+            result = self.run_query(query, fetch_all=True)
+            return result
+        except Exception as e:
+            logger.error(f"❌ Failed to fetch students: {e}")
+            return []
+        
+    def get_university_students_by_private_key(self, private_key:str):
+        """Fetch all students of a university by university private key"""
+        try:
+            hash_private_key = bcrypt.hashpw(private_key.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+            query = f"""
+            SELECT s.student_id, s.name, s.email, s.roll_no, s.dob, s.passed_out_year, s.created_at
+            FROM students s JOIN universities u ON s.univ_id = u.univ_id
+            WHERE u.private_key = '{hash_private_key}';
+            """
+            result = self.run_query(query, fetch_all=True)
+            return result
+        except Exception as e:
+            logger.error(f"❌ Failed to fetch students: {e}")
+            return []
+
+    def get_university_website_by_private_key(self, private_key:str) -> str:
+        """Fetch a university's website by private key"""
+        try:
+            hash_private_key = bcrypt.hashpw(private_key.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+            query = f"SELECT university_website FROM universities WHERE private_key = '{hash_private_key}';"
+            result = self.run_query(query, fetch_one=True)
+            return result[0] if result else None
+        except Exception as e:
+            logger.error(f"❌ Failed to fetch university website: {e}")
+            return None
+
+    def get_university_affiliate_colleges_by_univ_id(self, univ_id:int):
+        """Fetch all affiliate colleges of a university by university ID"""
+        try:
+            query = f"""
+            SELECT clg_code, clg_name, clg_address, clg_web, created_at
+            FROM affiliate_colleges
+            WHERE univ_id = {univ_id};
+            """
+            result = self.run_query(query, fetch_all=True)
+            return result
+        except Exception as e:
+            logger.error(f"❌ Failed to fetch affiliate colleges: {e}")
+            return []
+    
+    def get_university_affiliate_colleges_by_private_key(self, private_key:str):    
+        """Fetch all affiliate colleges of a university by university private key"""
+        try:
+            hash_private_key = bcrypt.hashpw(private_key.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+            query = f"""
+            SELECT ac.clg_code, ac.clg_name, ac.clg_address, ac.clg_web, ac.created_at
+            FROM affiliate_colleges ac JOIN universities u ON ac.univ_id = u.univ_id
+            WHERE u.private_key = '{hash_private_key}';
+            """
+            result = self.run_query(query, fetch_all=True)
+            return result
+        except Exception as e:
+            logger.error(f"❌ Failed to fetch affiliate colleges: {e}")
+            return []
+    
+    # ================================ End of University Management ===================================
+
+    # ===============================  ===================================
+
+
+
     def close(self):
         """Close cursor and connection"""
         if self.cursor:
@@ -285,8 +589,7 @@ class SupabaseDB:
             self.connection.close()
         print("🔒 Connection closed.")
 
-
-
+    
 if __name__ == "__main__":
     db = SupabaseDB()
     db.connect()
